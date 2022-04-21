e0b7292f5a4a19779f2d758f6fd719d68ed79231
migdracios <migdracios@gmail.com>
2022-04-21 13:58:25 +0900

    rollback initial settings

--- static/js/js.js
+++ static/js/js.js
@@ -1,11 +1,21 @@
 $(document).ready(function () {
     now_status(), sizing()
+    $(".icon_member").hover(function () {
+        $(".icon_member_textbox").show()
+    },
+        function () {
+            $(".icon_member_textbox").hide()
+        })
+
+    console.log('js test')
 })
+
 
 function now_status() {
     let today = new Date();
     let hours = today.getHours();
     console.log(hours)
+
 
     // 기능설계
     // MP부터 닳고 HP닳기
@@ -13,36 +23,27 @@ function now_status() {
     // 점심과 저녁시간 빼기
     // removeClass, addClass 활용
 
-    // 0시가 되면 초기화
-    if (hours==0 || hours <= 9) {
-        $('.hp_bar').removeClass('hp_0').addClass('hp_4')
-        $('.mp_bar').removeClass('mp_0').addClass('mp_4')
-    }
     // MP부터 10시부터~15시까지
 
-    else if (hours >= 10 && hours < 11) {
+    if (hours >= 10 && hours <= 11) {
         $('.mp_bar').removeClass('mp_4').addClass('mp_3')
-    } else if (hours >= 11 && hours < 12) {
-        $('.mp_bar').removeClass('mp_3').addClass('mp_2')
-    } else if (hours >= 12 && hours < 14) {
+    } else if (hours >= 11 && hours <= 12) {
+        $('.mp_bar').removeClass('mp_3').addClass('mp_3')
+    } else if (hours >= 12 && hours <= 13) {
         $('.mp_bar').removeClass('mp_2').addClass('mp_1')
-    } else if (hours >= 14 && hours < 15) {
+    } else if (hours >= 14 && hours <= 15) {
         $('.mp_bar').removeClass('mp_1').addClass('mp_0')
     }
 
     // HP 15시부터~21시까지
-    else if (hours >= 15 && hours < 16) {
+    else if (hours >= 15 && hours <= 16) {
         $('.hp_bar').removeClass('hp_4').addClass('hp_3')
-        $('.mp_bar').removeClass('mp_4').addClass('mp_0')
-    } else if (hours >= 16 && hours < 17) {
+    } else if (hours >= 16 && hours <= 17) {
         $('.hp_bar').removeClass('hp_3').addClass('hp_2')
-        $('.mp_bar').removeClass('mp_4').addClass('mp_0')
-    } else if (hours >= 17 && hours < 19) {
+    } else if (hours >= 17 && hours <= 18) {
         $('.hp_bar').removeClass('hp_2').addClass('hp_1')
-        $('.mp_bar').removeClass('mp_4').addClass('mp_0')
-    } else if (hours >= 19 && hours < 20) {
+    } else if (hours >= 19 && hours <= 20) {
         $('.hp_bar').removeClass('hp_1').addClass('hp_0')
-        $('.mp_bar').removeClass('mp_4').addClass('mp_0')
 
     } else {
         $('.hp_bar').removeClass('hp_1').addClass('hp_0')
@@ -53,25 +54,25 @@ function now_status() {
 function goToScroll_joo() {
     var location = document.querySelector("#joo").offsetTop;
     console.log(location)
-    window.scrollTo({top: location, behavior: 'smooth'});
+    window.scrollTo({ top: location, behavior: 'smooth' });
 }
 
 function goToScroll_lee() {
     var location = document.querySelector("#lee").offsetTop;
     console.log(location)
-    window.scrollTo({top: location, behavior: 'smooth'});
+    window.scrollTo({ top: location, behavior: 'smooth' });
 }
 
 function goToScroll_kim() {
     var location = document.querySelector("#kim").offsetTop;
     console.log(location)
-    window.scrollTo({top: location, behavior: 'smooth'});
+    window.scrollTo({ top: location, behavior: 'smooth' });
 }
 
 function goToScroll_hwang() {
     var location = document.querySelector("#hwang").offsetTop;
     console.log(location)
-    window.scrollTo({top: location, behavior: 'smooth'});
+    window.scrollTo({ top: location, behavior: 'smooth' });
 }
 
 function sizing() {
@@ -80,9 +81,10 @@ function sizing() {
 
 function music() {
     $('.mybtn').hide()
-
     const audioContainer = document.querySelector('#audioContainer');
     audioContainer.loop = true
     audioContainer.play()
+    $('.charimg').css('display', 'block');
+    $('#char').addClass('ani_fade_in_left')
 }
 
